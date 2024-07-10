98447db0e59ec907f4d965a204c4506207a8677a
Ahmad Abdur Rahman Khan <163046609+ahmadkhan100@users.noreply.github.com>
2024-07-10 03:48:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ class BlackScholes:
 with st.sidebar:
     st.title("📊 Black-Scholes Model")
     st.write("`Created by:`")
-    linkedin_url = "linkedin.com/in/ahmad-khan-b6584b225"
+    linkedin_url = "https://www.linkedin.com/in/ahmad-khan-b6584b225/"
     st.markdown(
         f'<a href="{linkedin_url}" target="_blank" style="text-decoration: none; color: inherit;"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="25" height="25" style="vertical-align: middle; margin-right: 10px;">`Khan, Ahmad`</a>',
         unsafe_allow_html=True)
